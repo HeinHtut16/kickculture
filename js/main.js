806db76275bbb5a9e60468a0db9a73495d59aca5
Hein Htut Aung <heinhtutaung@Heins-iMac.local>
2025-04-03 00:40:38 +0630

    Finish

--- js/main.js
+++ js/main.js
@@ -8,19 +8,22 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(response => response.text())
         .then(data => document.getElementById("footer-placeholder").innerHTML = data);
 
-    let index = 0;
-    const slides = document.querySelectorAll(".slide");
-    function showSlide(n) {
-        if (n >= slides.length) index = 0;
-        else if (n < 0) index = slides.length - 1;
-        else index = n;
-        document.querySelector(".slider").style.transform = `translateX(${-index * 100}%)`;
-    }
-    function nextSlide() { showSlide(index + 1); }
-    function prevSlide() { showSlide(index - 1); }
-    setInterval(nextSlide, 5000); // Auto-slide every 5 seconds
+    // let index = 0;
+    // const slides = document.querySelectorAll(".slide");
+    // function showSlide(n) {
+    //     if (n >= slides.length) index = 0;
+    //     else if (n < 0) index = slides.length - 1;
+    //     else index = n;
+    //     document.querySelector(".slider").style.transform = `translateX(${-index * 100}%)`;
+    // }
+    // function nextSlide() { showSlide(index + 1); }
+    // function prevSlide() { showSlide(index - 1); }
+    // setInterval(nextSlide, 5000);
 
     bidButton.addEventListener("click", () => {
         alert("Order is Complete!");
-    })
+    });
+
+    
+
 });
