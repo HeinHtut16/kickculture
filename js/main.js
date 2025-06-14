6e7369261269784648525bda438a9813786ff2d2
Hein Htut Aung <heinhtutaung@Heins-iMac.local>
2025-06-14 23:21:31 +0630

    Finish

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const bidButton = document.querySelector(".bid-button");
+    console.log(bidButton)
     fetch("/components/nav.html")
         .then(response => response.text())
         .then(data => document.getElementById("nav-placeholder").innerHTML = data);
